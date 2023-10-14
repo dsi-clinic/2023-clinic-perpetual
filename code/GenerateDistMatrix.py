5e1bce79bf3775e98df8a8e31e8495d96b197a5a
genieugod@gmail.com <genieugod@gmail.com>
2023-10-13 20:21:11 -0500

    add matrix result file

--- code/GenerateDistMatrix.py
+++ code/GenerateDistMatrix.py
@@ -11,7 +11,7 @@ import pickle
 
 def get_matrix_data(coordinates, access_token):
     """
-    Fetch travel time matrix using Mapbox Matrix API. 
+    Fetch travel time matrix using Mapbox Matrix API.
     Sets the first coordinate as the source and the rest as destinations.
 
     :param coordinates: List of coordinates [longitude, latitude]
@@ -22,7 +22,9 @@ def get_matrix_data(coordinates, access_token):
     coordinates_str = ";".join([f"{lon},{lat}" for lon, lat in coordinates])
 
     # Endpoint URL (assuming driving mode here, but can be changed to walking, cycling, etc.)
-    url = f"https://api.mapbox.com/directions-matrix/v1/mapbox/driving/{coordinates_str}"
+    url = (
+        f"https://api.mapbox.com/directions-matrix/v1/mapbox/driving/{coordinates_str}"
+    )
 
     # Parameters
     params = {
@@ -38,6 +40,7 @@ def get_matrix_data(coordinates, access_token):
     # Return the JSON response
     return response.json()
 
+
 def generate_capacity_list(df, timestamp_str):
     """
     Generate a list of capacities from the dataframe.
@@ -47,8 +50,8 @@ def generate_capacity_list(df, timestamp_str):
     """
     # save list as pkl file
     capacity_file = f"data/capacity_list_{timestamp_str}.pkl"
-    with open(capacity_file, 'wb') as f:
-        pickle.dump(list(map(int, df['Daily_Pickup_Totes'].tolist())), f)
+    with open(capacity_file, "wb") as f:
+        pickle.dump(list(map(int, df["Daily_Pickup_Totes"].tolist())), f)
 
 
 def initialize_data():
@@ -61,46 +64,49 @@ def initialize_data():
     # Initialize the parser
     config = configparser.ConfigParser()
     # Read the config file
-    config.read('config.ini')
-    mapbox_token = config['mapbox']['token']
+    config.read("config.ini")
+    mapbox_token = config["mapbox"]["token"]
 
     # Take file name from terminal
     file_name = sys.argv[1]
     df = pd.read_csv(file_name)
     # Convert coordinates to list of lists
-    df['Coordinates'] = df[['Longitude', 'Latitude']].values.tolist()
-    df.drop(columns=['Longitude', 'Latitude'], inplace=True)
+    df["Coordinates"] = df[["Longitude", "Latitude"]].values.tolist()
+    df.drop(columns=["Longitude", "Latitude"], inplace=True)
 
     return df, mapbox_token
 
 
 def main():
-
     # Initialize the data
     df, mapbox_token = initialize_data()
 
     # Initialize the matrix
     full_matrix = np.zeros(len(df))
 
-    # Get the matrix data. 
+    # Get the matrix data.
     # Goes through every source once and then every destination for every source.
-    col_idx = df.columns.get_loc('Coordinates')
+    col_idx = df.columns.get_loc("Coordinates")
     for i in tqdm.tqdm(range(len(df))):
         horizontal = [[]]
         # Goes through 24 destinations for every source due to api limit
-        for j in range(0, len(df), 24):
-            coordinate_list = [df.iloc[i, col_idx]] + df.iloc[j:j+24, col_idx].tolist()
-            result = get_matrix_data(coordinate_list, mapbox_token)['distances']
-            horizontal = np.hstack((horizontal, result))
+        for j in range(0, len(df), 13):
+            coordinate_list = [df.iloc[i, col_idx]] + df.iloc[
+                j : j + 13, col_idx
+            ].tolist()
+            result_dict = get_matrix_data(coordinate_list, mapbox_token)
+            result_list = result_dict["distances"]
+            horizontal = np.hstack((horizontal, result_list))
             time.sleep(1)
         full_matrix = np.vstack((full_matrix, horizontal))
 
     current_datetime = datetime.datetime.now()
-    timestamp_str = current_datetime.strftime('%Y%m%d_%H%M%S')
-    
+
+    timestamp_str = current_datetime.strftime("%Y%m%d_%H%M%S")
+
     # remove the first row which is all zeros
     full_matrix = full_matrix[1:, :]
-    
+
     # Save the matrix to a file
     filename = f"data/generated_distance_matrices/distance_matrix_{timestamp_str}.npy"
     np.save(filename, full_matrix)
@@ -108,7 +114,8 @@ def main():
     # Save the capacity list to a file
     generate_capacity_list(df, timestamp_str)
 
-    print('Complete!')
+    print("Complete!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
