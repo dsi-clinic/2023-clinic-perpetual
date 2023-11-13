34c43e1b69bff568e220c45f8298ccb5640c41e3
daihuanlin <daihuanlin@uchicago.edu>
2023-11-13 11:19:59 -0600

    adding support files (capacity and supplementary info extractors)

---
+++ code/extract_supplementary_info.py
@@ -0,0 +1,20 @@
+# produce supplementary info about each point (used for the popups during mapping)
+
+if __name__ == '__main__':
+    # read config
+    config = configparser.ConfigParser()
+    config.read('../utils/config.ini')
+    cfg = config['extract.supp_info']
+
+    # parse config
+    truth_df_path = cfg['truth_df_path']
+    supp_info_df_savepath = cfg['supp_info_df_savepath']
+    
+    # read single-truth dataframe
+    truth_df = pd.read_csv(truth_df_path)
+
+    # prepare supp_info dataframe
+    supp_info = truth_df[['Name', 'Daily_Pickup_Totes', 'Weekly_Dropoff_Totes', 'category', 'Address', 'location_type', 'pickup_type']]
+
+    # save supp_info dataframe
+    supp_info.to_csv(supp_info_df_savepath)
