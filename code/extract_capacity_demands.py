7dc1baa6364c6b46a1f9bb1f7ead5ef31e9067b4
daihuanlin <daihuanlin@uchicago.edu>
2023-11-14 10:25:22 -0600

    adding libraries

--- code/extract_capacity_demands.py
+++ code/extract_capacity_demands.py
@@ -1,20 +1,24 @@
 # produce capacity list from the single-truth dataframe (used for routing)
 
-if __name__ == '__main__':
+import configparser
+
+import pandas as pd
+
+if __name__ == "__main__":
     # read config
     config = configparser.ConfigParser()
-    config.read('../utils/config.ini')
-    cfg = config['extract.capacity_demands']
+    config.read("../utils/config.ini")
+    cfg = config["extract.capacity_demands"]
 
     # parse config
-    truth_df_path = cfg['truth_df_path']
-    capacity_df_savepath = cfg['capacity_df_savepath']
-    
+    truth_df_path = cfg["truth_df_path"]
+    capacity_df_savepath = cfg["capacity_df_savepath"]
+
     # read single-truth dataframe
     truth_df = pd.read_csv(truth_df_path)
 
     # prepare capacity dataframe
-    capacities = truth_df[['Daily_Pickup_Totes', 'Weekly_Dropoff_Totes']]
+    capacities = truth_df[["Daily_Pickup_Totes", "Weekly_Dropoff_Totes"]]
 
     # save capacity dataframe
     capacities.to_csv(capacity_df_savepath)
