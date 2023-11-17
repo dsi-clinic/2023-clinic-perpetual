c6c5b14837200d31057f3d08bb2278bae3688a49
HuanlinDai <dai.huan.lin@gmail.com>
2023-11-16 22:57:42 -0600

    modifying code to output dictionaries in csv form

--- code/extract_capacity_demands.py
+++ code/extract_capacity_demands.py
@@ -1,8 +1,8 @@
 # produce capacity list from the single-truth dataframe (used for routing)
 
 import configparser
-
 import pandas as pd
+import csv
 
 if __name__ == "__main__":
     # read config
@@ -18,7 +18,15 @@ if __name__ == "__main__":
     truth_df = pd.read_csv(truth_df_path)
 
     # prepare capacity dataframe
-    capacities = truth_df[["Daily_Pickup_Totes", "Weekly_Dropoff_Totes"]]
+    capacities = {}
+    name_col = 'Name'
+    demand_cols = ["Daily_Pickup_Totes", "Weekly_Dropoff_Totes"]
 
-    # save capacity dataframe
-    capacities.to_csv(capacity_df_savepath)
+    # prepare capacity dictionary
+    for i in range(len(truth_df)):
+        capacities[truth_df[name_col].iloc[i]] = truth_df[demand_cols].iloc[i]
+
+    with open('../output/data/extracted_capacities_dict.csv', 'w') as f:
+        w = csv.DictWriter(f, capacities.keys())
+        w.writeheader()
+        w.writerow(capacities)
--- code/extract_supplementary_info.py
+++ code/extract_supplementary_info.py
@@ -1,26 +1,28 @@
 # produce supplementary info about point (used for the map popups)
 
 import configparser
-
 import pandas as pd
+import csv
+
+
 
 if __name__ == "__main__":
     # read config
     config = configparser.ConfigParser()
     config.read("../utils/config.ini")
     cfg = config["extract.supp_info"]
-
+    
     # parse config
     truth_df_path = cfg["truth_df_path"]
     supp_info_df_savepath = cfg["supp_info_df_savepath"]
 
     # read single-truth dataframe
     truth_df = pd.read_csv(truth_df_path)
-
-    # prepare supp_info dataframe
-    supp_info = truth_df[
-        [
-            "Name",
+    
+    # prepare supp_info dictionary
+    supp_info = {}
+    name_col = 'Name'
+    supp_cols = [
             "Daily_Pickup_Totes",
             "Weekly_Dropoff_Totes",
             "category",
@@ -28,7 +30,10 @@ if __name__ == "__main__":
             "location_type",
             "pickup_type",
         ]
-    ]
+    for i in range(len(truth_df)):
+        supp_info[truth_df[name_col].iloc[i]] = truth_df[supp_cols].iloc[i]
 
-    # save supp_info dataframe
-    supp_info.to_csv(supp_info_df_savepath)
+    with open('../output/data/extracted_supp_info_dict.csv', 'w') as f:
+        w = csv.DictWriter(f, supp_info.keys())
+        w.writeheader()
+        w.writerow(supp_info)
