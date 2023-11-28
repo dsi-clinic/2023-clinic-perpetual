c763cf9952f2e41e367b1b52e6214ca7678594cf
genieugod@gmail.com <genieugod@gmail.com>
2023-11-28 00:27:40 -0600

    pre-commit

--- code/GenerateSingleSourceofTruth.py
+++ code/GenerateSingleSourceofTruth.py
@@ -6,9 +6,10 @@ Created on Mon Nov 27 01:17:30 2023
 @author: genie_god
 """
 
-import pandas as pd
-import configparser
 import ast
+import configparser
+
+import pandas as pd
 
 
 def generate_single_source_of_truth():
@@ -25,34 +26,32 @@ def generate_single_source_of_truth():
     df_outdoor = pd.read_csv(file_name_outdoor)
 
     # update location_type and pickup_type for each dataset
-    df_indoor.loc[:, "location_type"] = 'indoor'
-    df_indoor.loc[:, "pickup_type"] = 'truck'
+    df_indoor.loc[:, "location_type"] = "indoor"
+    df_indoor.loc[:, "pickup_type"] = "truck"
     df_outdoor.loc[:, "Daily_Pickup_Totes"] = 1.0
     df_outdoor.loc[:, "Weekly_Dropoff_Totes"] = 0.0
     df_outdoor.loc[:, "location_type"] = "outdoor"
 
     # prepare data for analyze
-    df_outdoor = df_outdoor.rename(columns={"longitude": "Longitude",
-                                            "latitude": "Latitude"})
+    df_outdoor = df_outdoor.rename(
+        columns={"longitude": "Longitude", "latitude": "Latitude"}
+    )
 
     # add tote number info
     # when the indoor and oudoor df don't have pickup/dropoff totes number
-    if 'Weekly_Dropoff_Totes' not in df_indoor.columns:
+    if "Weekly_Dropoff_Totes" not in df_indoor.columns:
         old_galveston = pd.read_csv("../data/FUE_Galveston.csv")
-        galveston_sub = old_galveston.loc[:, ["Name",
-                                              "Weekly_Dropoff_Totes",
-                                              "Daily_Pickup_Totes"]]
-        df_indoor = pd.merge(df_indoor,
-                             galveston_sub,
-                             on="Name",
-                             how='left')
+        galveston_sub = old_galveston.loc[
+            :, ["Name", "Weekly_Dropoff_Totes", "Daily_Pickup_Totes"]
+        ]
+        df_indoor = pd.merge(df_indoor, galveston_sub, on="Name", how="left")
 
     # add source loacation
-    source_location = \
-        ast.literal_eval(config["original data source"]["source"])
+    source_location = ast.literal_eval(config["original data source"]["source"])
 
     df_source = pd.DataFrame(
-        {'Longitude': [source_location[0]], 'Latitude': [source_location[1]]})
+        {"Longitude": [source_location[0]], "Latitude": [source_location[1]]}
+    )
 
     single_source_truth = pd.concat([df_source, df_indoor, df_outdoor])
 
@@ -61,14 +60,14 @@ def generate_single_source_of_truth():
     single_source_truth.to_csv(filename, index=False)
 
     # Ensure the 'Matrix Dir' section exists
-    if 'original data source' not in config:
-        config['original data source'] = {}
+    if "original data source" not in config:
+        config["original data source"] = {}
 
     # Assign the filename to the single_source_of_truth section
-    config['original data source']['single_source_of_truth'] = filename
+    config["original data source"]["single_source_of_truth"] = filename
 
     # Write the configuration to an INI file
-    with open('../utils/config_inputs.ini', 'w') as configfile:
+    with open("../utils/config_inputs.ini", "w") as configfile:
         config.write(configfile)
 
     print(f"single source of truth generated under the file {filename}")
