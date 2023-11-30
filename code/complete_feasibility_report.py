bbe0b4224a2722723931997026e1f556562aff06
Sarah Ann Walker <sarah@Sarahs-MacBook-Pro-4.local>
2023-11-30 17:31:34 -0600

    made updates to the feasibility report, the code that creates the feasibility report, and the config arguments

--- code/complete_feasibility_report.py
+++ code/complete_feasibility_report.py
@@ -13,37 +13,36 @@ import configparser
 import pandas as pd
 
 
-
 def extract_route_info(path_to_routes, num_routes):
-    '''
-    Use this function to open each route from the output 
+    """
+    Use this function to open each route from the output
     to extract total loads, locations, and distances for each route
 
     inputs:
     outputs: list of lists - [list of load_per_vehicle,
                             list of locations_per_vehicle,
                             list of distance_per_vehicle]
-    '''
+    """
     load_list = []
     num_loc_list = []
     distance_list = []
     for i in range(num_routes):
-        route = pd.read_csv(path_to_routes + "/route" + str(i+1))
-        load_list.append(route.loc[len(route) -1, "Truck_Load"])
-        num_loc_list.append(len(route) -2)
-        distance_list.append(route.loc[len(route) -1, "Cumulative_Distance"])
+        route = pd.read_csv(path_to_routes + "/route" + str(i + 1))
+        load_list.append(route.loc[len(route) - 1, "Truck_Load"])
+        num_loc_list.append(len(route) - 2)
+        distance_list.append(route.loc[len(route) - 1, "Cumulative_Distance"])
 
     return load_list, num_loc_list, distance_list
 
 
 def build_trial_dict(feasibility_report):
-    '''
-    Build a dictionary so every key matches the order of the 
+    """
+    Build a dictionary so every key matches the order of the
     columns of the feasibility file
 
     input: df - feasibility_report
     output: dict - trial_dict
-    '''
+    """
     trial_dict = {}
     list_of_column_names = feasibility_report.columns.to_list()
     for name in list_of_column_names:
@@ -54,67 +53,60 @@ def build_trial_dict(feasibility_report):
 def main():
     trial_dict = build_trial_dict(feasibility_report)
 
-    #create arguments to the extract_route_info function
-    path_to_routes = config["optimize google cvrp"][
-    "output_path"
+    # create arguments to the extract_route_info function
+    path_to_routes = config["optimize google cvrp"]["output_path"]
+    num_routes = config["optimize google cvrp"]["num_vehicles"].astype(int)
+
+    # use these arguments to call the function extract_route_info
+    (
+        load_per_vehicle,
+        locations_per_vehicle,
+        distance_per_vehicle,
+    ) = extract_route_info(path_to_routes, num_routes)
+
+    # assign values to appropriate key of the trial dictionary
+    trial_dict["output_path"] = path_to_routes
+    trial_dict["num_vehicles"] = num_routes
+
+    trial_dict["load_per_vehicle"] = load_per_vehicle
+    trial_dict["locations_per_vehicle"] = locations_per_vehicle
+    trial_dict["distance_per_vehicle"] = distance_per_vehicle
+
+    trial_dict["cumulative_load"] = sum(load_per_vehicle)
+    trial_dict["total_num_locations"] = sum(locations_per_vehicle)
+    trial_dict["cumulative_distance"] = sum(distance_per_vehicle)
+
+    # assign values to remaining keys of the trial dictionary
+    # by grabbing the arguments from the config file
+    trial_dict["trial_name"] = config["feasibility report"]["trial_name"]
+    trial_dict["description"] = config["feasibility report"]["description"]
+    trial_dict["path_to_distance_matrix"] = config["optimize google cvrp"][
+        "path_to_distance_matrix"
     ]
-    num_routes = config["optimize google cvrp"][
-    "num_vehicles"
+    trial_dict["path_to_dataframe"] = config["optimize google cvrp"][
+        "path_to_dataframe"
+    ]
+    trial_dict["simulation_run_time"] = config["optimize google cvrp"][
+        "num_seconds_simulation"
+    ].astype(int)
+    trial_dict["vehicle_type"] = config["feasibility report"]["vehicle_type"]
+    trial_dict["vehicle_capacity"] = config["optimize google cvrp"][
+        "vehicle_capacity"
     ].astype(int)
 
-    #use these arguments to call the function extract_route_info
-    load_per_vehicle, locations_per_vehicle, distance_per_vehicle = extract_route_info(path_to_routes, num_routes) 
-
-    #assign values to appropriate key of the trial dictionary
-    trial_dict['output_path'] = path_to_routes
-    trial_dict['num_vehicles'] = num_routes
-
-    trial_dict['load_per_vehicle'] = load_per_vehicle
-    trial_dict['locations_per_vehicle'] = locations_per_vehicle
-    trial_dict['distance_per_vehicle'] = distance_per_vehicle
-    
-    trial_dict['cumulative_load'] = sum(load_per_vehicle)
-    trial_dict['total_num_locations'] = sum(locations_per_vehicle)
-    trial_dict['cumulative_distance'] = sum(distance_per_vehicle)
-
-    #assign values to remaining keys of the trial dictionary
-    #by grabbing the arguments from the config file
-    trial_dict['trial_name'] = config["feasibility report"][
-    "trial_name"
-    ]  
-    trial_dict['description'] = config["feasibility report"][
-    "description"
-    ]
-    trial_dict['path_to_distance_matrix'] = config["optimize google cvrp"][
-    "path_to_distance_matrix"
-    ]
-    trial_dict['path_to_dataframe'] = config["optimize google cvrp"][
-    "path_to_dataframe"
-    ]
-    trial_dict['simulation_run_time'] = config["optimize google cvrp"][
-    "num_seconds_simulation"
-    ].astype(int)
-    trial_dict['vehicle_type'] = config["feasibility report"][
-    "vehicle_type"
-    ]
-    trial_dict['vehicle_capacity'] = config["optimize google cvrp"][
-    "vehicle_capacity"
-    ].astype(int)
-    
     # Need to include time and cost when we can update our vrp parameters
     # #trial_dict['time_per_vehicle']
     # #trial_dict['cost_per_vehicle']
     # #trial_dict['cumulative_time']
     # #trial_dict['cumulative_cost']
-    
-    #trial_dict['path_to_visualizations'] 
-    #trial_dict['name_of_visualizations']
 
-    #save the trial dictionary as a new row in the feasibility file
-    feasibility_report.append(trial_dict,ignore_index=True)
-    #save the feasibility file in place
+    # trial_dict['path_to_visualizations']
+    # trial_dict['name_of_visualizations']
+
+    # save the trial dictionary as a new row in the feasibility file
+    feasibility_report.append(trial_dict, ignore_index=True)
+    # save the feasibility file in place
     feasibility_report.to_csv("../output/feasibilityfile.csv")
-
 
 
 if __name__ == "__main__":
@@ -123,7 +115,7 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("path/config_inputs.ini")
 
-    #open the feasibility file so you can make edits 
+    # open the feasibility file so you can make edits
     feasibility_report = pd.read_csv("../output/feasibilityfile.csv")
 
     main()
