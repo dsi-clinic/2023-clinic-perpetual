fc27315d6f9972b8b58b734e3506ee9e00871963
daihuanlin <daihuanlin@uchicago.edu>
2023-12-02 18:08:44 -0600

    formatting fixes

--- code/bike_conversion.py
+++ code/bike_conversion.py
@@ -5,6 +5,7 @@ import folium
 import numpy as np
 import pandas as pd
 
+
 def read_config(path, section):
     """
     Read in a config
@@ -17,17 +18,19 @@ def read_config(path, section):
     config = configparser.ConfigParser()
     config.read(path)
     return config[section]
-    
+
+
 def filter_df(df, col, val):
     """
     Filter for rows with certain values in the desired column in df
 
     Parameters:
-        df : pd.DataFrame 
+        df : pd.DataFrame
         col : string
         val : value of interest
     """
     return df[df[col] == val]
+
 
 def filter_dists(dist_mtrx, from_ind, to_ind):
     """
@@ -41,6 +44,7 @@ def filter_dists(dist_mtrx, from_ind, to_ind):
     str_inds = [str(i) for i in to_ind]
     res = dist_mtrx[str_inds].loc[from_ind]
     return res
+
 
 def add_markers(f_map, points, color="blue"):
     """
@@ -104,7 +108,6 @@ if __name__ == "__main__":
     # 1. read config
     cfg = read_config("../utils/config_inputs.ini", "convert.to_bikes")
 
-    
     # 2. parse config
     truth_df_path = cfg["truth_df_path"]
     truth_dist_df_path = cfg["truth_dist_df_path"]
@@ -117,12 +120,11 @@ if __name__ == "__main__":
     truck_dist_df_savepath = cfg["truck_dist_df_savepath"]
     bike_dist_df_savepath = cfg["bike_dist_df_savepath"]
     bike_aggregates_savedir = cfg["bike_aggregates_savedir"]
-    
+
     map_all_save_path = cfg["map_all_save_path"]
     map_bikes_save_path = cfg["map_bikes_save_path"]
     map_trucks_save_path = cfg["map_trucks_save_path"]
 
-    
     # 3.0 read single-truth dataframe and single-truth distance matrix
     truth_df = pd.read_csv(truth_df_path)
     truth_dist = pd.read_csv(truth_dist_df_path)
@@ -131,33 +133,32 @@ if __name__ == "__main__":
     for i in aggs:
         converted_truth_df.at[i, "pickup_type"] = "Bike_Aggregate"
 
-    
     # 4. get distances between bike-served and truck-served locations
-    bike_ind = filter_df(converted_truth_df, "pickup_type", "Bike").index.to_list()
-    truck_ind = filter_df(converted_truth_df, "pickup_type", "Truck").index.to_list()
+    bike_ind = filter_df(
+        converted_truth_df, "pickup_type", "Bike"
+    ).index.to_list()
+    truck_ind = filter_df(
+        converted_truth_df, "pickup_type", "Truck"
+    ).index.to_list()
     bike_to_truck_dists = filter_dists(truth_dist, bike_ind, truck_ind)
 
-    
     # 5. mark truck-served points within {distance_thresh} of bike-served points
     converts_ind = []
     for col in bike_to_truck_dists.columns:
         if min(bike_to_truck_dists[col]) <= distance_thresh:
             converts_ind.append(int(col))
 
-    
     # 6. convert marked truck-serviced locations to bike-servicable locations
     for i in converts_ind:
         if i not in aggs:
             converted_truth_df.at[i, "pickup_type"] = "Bike_Converted"
 
-    
     # 7.0. collecting indices for aggregating (7.1.) and outputs (8.)
     total_ind = {i for i in range(len(truth_df))}
     bike_no_agg_ind = sorted(bike_ind + converts_ind)
     bike_all_ind = sorted(bike_no_agg_ind + aggs)
     truck_all_ind = sorted(list(total_ind - set(bike_no_agg_ind)))
 
-    
     # 7.1. assign aggregate locations for each bike point
     agg_to_bike_dists = filter_dists(truth_dist, aggs, bike_no_agg_ind)
     agg_assignments = {}
@@ -169,7 +170,6 @@ if __name__ == "__main__":
             assignment, "Daily_Pickup_Totes"
         ] += converted_truth_df.at[key, "Daily_Pickup_Totes"]
 
-    
     # 8. prepare info dataframes for output
     truck_converted_df = converted_truth_df[
         (converted_truth_df["pickup_type"] == "Truck")
@@ -184,14 +184,12 @@ if __name__ == "__main__":
     truck_dist_df = filter_dists(truth_dist, truck_all_ind, truck_all_ind)
     bike_dist_df = filter_dists(truth_dist, bike_all_ind, bike_all_ind)
 
-    
     # 9. export dataframes
     truck_converted_df.to_csv(truck_df_savepath)
     bike_converted_df.to_csv(bike_df_savepath)
     truck_dist_df.to_csv(truck_dist_df_savepath)
     bike_dist_df.to_csv(bike_dist_df_savepath)
 
-    
     # 10. visualize
     trucks = converted_truth_df[converted_truth_df["pickup_type"] == "Truck"]
     bikes = converted_truth_df[converted_truth_df["pickup_type"] == "Bike"]
